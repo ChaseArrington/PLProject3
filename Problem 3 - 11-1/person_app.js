b5b5f25ff616ff541ddda58d3128f0f09123fe94
Chase Arrington <chasearrington1@gmail.com>
2016-03-07 23:49:35 -0600

    Finished html of Problem 3

---
+++ Problem 3 - 11-1/person_app.js
@@ -0,0 +1,27 @@
+/**
+ * Created by chase_000 on 3/7/2016.
+ */
+
+var person = function(){
+    var data = {
+        name
+    }
+}();
+
+var customer = function() {
+    var data = {
+
+    }
+}
+
+document.writeln("Welcome to the Person Tester Application");
+
+document.writeln("You entered:");
+document.writeln("Name: " + );
+document.writeln("Email: " + );
+document.writeln("Customer Number: " + );
+
+document.writeln("You entered:");
+document.writeln("Name: " + );
+document.writeln("Email: " + );
+document.writeln("Social security number: " + );
